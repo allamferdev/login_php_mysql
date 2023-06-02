95cc8ed03cd5dbda4a05f055e217263791ba5c84
Allam Ferreira <allamfsantos@gmail.com>
2023-06-02 16:30:55 -0300

    página finalizada

--- index.php
+++ index.php
@@ -6,15 +6,21 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sistema de Login</title>
+    <!--Estilo-->
+    <link rel="stylesheet" href="assets/style.css">
     <!--Boostrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
-<body>
+<body style="background-color:#f2f2f2">
+<div class="login">
 <div class="container">
     <div class="row">
         <div class="col-lg-4 offset-lg-4">
           <div class="card">
               <div class="card-body">
+                  <div id="logo-ctn">
+                  <img src="assets/logo.png" alt="logo" id="logo">
+                  </div>
                   <form action="login.php" method="POST">
                       <div>
                           <div class="mb-3">
@@ -38,6 +44,7 @@
           </div>
         </div>
     </div>
+</div>
 </div>
 </body>
 </html>
