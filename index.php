2ea723eabf326dc4f7492516ffd234a9347496c6
Allam Ferreira <allamfsantos@gmail.com>
2023-06-03 13:12:09 -0300

    arquivo para o banco de dados

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <!--Boostrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
-<body style="background-color:#f2f2f2">
+<body style="background-color:#31404A">
 <div class="login">
 <div class="container">
     <div class="row">
